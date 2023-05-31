b2e21eb6f8a1484c7aeb6a1062772a6701e4be3e
Lorenzo <lorenzo.acetii@gmail.com>
2023-05-31 19:49:21 -0300

    Create playerdata state and handle change name

--- client/src/contexts/general.js
+++ client/src/contexts/general.js
@@ -3,6 +3,55 @@ import { createContext, useState } from "react"
 export const GeneralContext = createContext({})
 
 export const GeneralProvider = ({ children }) => {
+
+    const [playerData, setPlayerData] = useState({
+        player1: {
+            name: "Player 1",
+            theme: "rose",
+            coins: 0,
+            activities: [
+                {
+                    id: 1,
+                    name: "Assistir Star Wars",
+                    reroll_cost: 1,
+                    weight: 10
+                },
+                {
+                    id: 2,
+                    name: "Jogar God of War",
+                    reroll_cost: 1,
+                    weight: 10
+                }
+            ]
+        },
+
+        player2: {
+            name: "Player 2",
+            theme: "cyan",
+            coins: 0,
+            activities: [
+                {
+                    id: 1,
+                    name: "Assistir Game of Thrones",
+                    reroll_cost: 1,
+                    weight: 10
+                },
+                {
+                    id: 2,
+                    name: "Assistir Xuxa",
+                    reroll_cost: 1,
+                    weight: 10
+                },
+                {
+                    id: 3,
+                    name: "Jantar no Gendai",
+                    reroll_cost: 1,
+                    weight: 10
+                },
+            ]
+        },
+    })
+
     const [activities, setActivities] = useState({
         player1:
             [{ id: 1, name: 'Activity 1' }, { id: 2, name: 'Activity 2' }],
@@ -47,7 +96,8 @@ export const GeneralProvider = ({ children }) => {
     return (
         <GeneralContext.Provider value={{
             activities, setActivities,
-            theme, setTheme, playerName, setPlayerName, findHighestId, getActivityIndex
+            theme, setTheme, playerName, setPlayerName, findHighestId, getActivityIndex,
+            playerData, setPlayerData
         }}>
             {children}
         </GeneralContext.Provider>
--- client/src/pages/GameConfiguration/index.jsx
+++ client/src/pages/GameConfiguration/index.jsx
@@ -4,9 +4,10 @@ import useGeneral from '../../hooks/useGeneral'
 
 export default function GameConfiguration() {
 
-    const { theme, playerName, setPlayerName } = useGeneral()
-
-    const handleChange = e => setPlayerName({ ...playerName, [e.target.name]: e.target.value })
+    const { playerData, setPlayerData } = useGeneral()
+    
+    const handleChangeName = e => 
+        setPlayerData({ ...playerData, [e.target.name]: {...e.target.name, name: e.target.value} })
 
     const handleKeyDown = e => {
         if (e.key === 'Enter') {
@@ -15,7 +16,7 @@ export default function GameConfiguration() {
     }
 
     const handleBlur = () => {
-        // API save
+        // Local Storage save
     }
 
 
@@ -25,35 +26,35 @@ export default function GameConfiguration() {
             <C.PageContent>
                 <C.Main>
                     <C.Section>
-                        <C.PlayerContainer theme={theme.player1}>
+                        <C.PlayerContainer theme={playerData.player1.theme}>
                             <C.PlayerInput
                                 name='player1'
-                                value={playerName.player1}
-                                onChange={handleChange}
+                                value={playerData.player1.name}
+                                onChange={handleChangeName}
                                 onKeyDown={handleKeyDown}
                                 onClick={e => e.target.select()}
                                 onBlur={handleBlur}
                             />
                         </C.PlayerContainer>
 
-                        <C.ActivitiesContainer theme={theme.player1}>
+                        <C.ActivitiesContainer theme={playerData.player1.theme}>
                             <ActivitiesList player='player1' />
                         </C.ActivitiesContainer>
                     </C.Section>
 
                     <C.Section>
-                        <C.PlayerContainer theme={theme.player2}>
+                        <C.PlayerContainer theme={playerData.player2.theme}>
                             <C.PlayerInput
                                 name='player2'
-                                value={playerName.player2}
-                                onChange={handleChange}
+                                value={playerData.player2.name}
+                                onChange={handleChangeName}
                                 onKeyDown={handleKeyDown}
                                 onClick={e => e.target.select()}
                                 onBlur={handleBlur}
                             />
                         </C.PlayerContainer>
 
-                        <C.ActivitiesContainer theme={theme.player2}>
+                        <C.ActivitiesContainer theme={playerData.player2.theme}>
                             <ActivitiesList player='player2' />
                         </C.ActivitiesContainer>
                     </C.Section>
