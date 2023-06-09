0867df4fe9e77df03c1a719c5f8d9e291014398f
Lorenzo <lorenzo.acetii@gmail.com>
2023-06-09 17:06:19 -0300

    Create footer with dev name

---
+++ client/src/pages/MainPage/Footer/index.jsx
@@ -0,0 +1,9 @@
+import * as C from './styles'
+
+export default function Footer() {
+    return (
+        <C.Footer>
+            Couple Wheel - Lorenzo Aceti - 2023
+        </C.Footer>
+    )
+}
---
+++ client/src/pages/MainPage/Footer/styles.jsx
@@ -0,0 +1,14 @@
+import tw from 'tailwind-styled-components'
+
+export const Footer = tw.footer`
+    flex 
+    items-center 
+    justify-center
+    fixed 
+    bottom-0 
+    left-0 
+    w-full 
+    h-10
+    font-bold
+    opacity-30
+`
