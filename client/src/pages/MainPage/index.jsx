911d9238880766aa16f3d0c2a5aba2869616386f
Lorenzo <lorenzo.acetii@gmail.com>
2023-06-11 18:03:55 -0300

    Change containers position based on viewport size

--- client/src/pages/MainPage/index.jsx
+++ client/src/pages/MainPage/index.jsx
@@ -1,6 +1,7 @@
 import * as C from './styles'
 import GameConfiguration from './GameConfiguration'
 import Roulette from './Roulette'
+import { useState } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import useMediaQuery from '../../hooks/useMediaQuery'
