6ab4fff809d8b24b318073cef40a693a2636fc6e
Lorenzo <lorenzo.acetii@gmail.com>
2023-06-11 20:35:28 -0300

    Created a timer for the message

--- client/src/components/Message/index.jsx
+++ client/src/components/Message/index.jsx
@@ -1,13 +1,29 @@
 import * as C from './styles'
 import usePlayer from '../../hooks/usePlayer'
+import { useEffect } from 'react'
 
 export default function Message() {
     const { setMessage, message } = usePlayer()
 
+    useEffect(() => {
+        let timerId
+
+        if (message?.text?.length > 0) {
+            timerId = setTimeout(() => {
+                setMessage({message: '', type: ''})
+            }, 5000)
+        }
+
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [message])
+
+
     return (
         <>
             {
-                message.text.length > 0 &&
+                message?.text?.length > 0 &&
                 <C.Message type={message.type} onClick={() => setMessage({ text: '', type: '' })}>
                     {message.text}
                 </C.Message>
